ac21894c76355bf78d17ffb57a1e5cf7de12ef3f
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 20:05:50 -0300

    Adicionando exercicio02

---
+++ exercicio/src/exercicios02.java
@@ -0,0 +1,13 @@
+public class exercicios02 {
+    public static void main(String[] args) {
+
+        int soma =0;
+        int num = 5;
+        for (int i = 0; i<= num; i++){
+            soma += i;
+        }
+
+        System.out.println(soma);
+
+        }
+}
