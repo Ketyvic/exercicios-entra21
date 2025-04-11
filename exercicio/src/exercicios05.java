b4fbc9f133265fe7b099f2aa45ee128571c36ed5
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 20:19:08 -0300

    adicionando exercicio05

---
+++ exercicio/src/exercicios05.java
@@ -0,0 +1,15 @@
+public class exercicios05 {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int i = 1;
+        while ( i <= n) {
+            System.out.println(i);
+            i+= 2;
+
+
+        }
+
+
+    }
+}
