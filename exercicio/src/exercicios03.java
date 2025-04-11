dc0f026bbdfbb9e5afc89ecac778062fb4fc53a4
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 20:13:50 -0300

    adicionando exercicio03

---
+++ exercicio/src/exercicios03.java
@@ -0,0 +1,10 @@
+public class exercicios03 {
+    public static void main(String[] args) {
+
+        int tab= 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tab + "x" +i+ "="+(tab*i));
+        }
+
+    }
+}
