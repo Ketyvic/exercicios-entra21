17e77f476dd430eaf676974ee084111d4e79cfa5
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 21:30:21 -0300

    adicionando exercicio10

---
+++ exercicio/src/exercicios010.java
@@ -0,0 +1,18 @@
+public class exercicios010 {
+    public static void main(String[] args) {
+
+        int contagem = 10;
+        for ( int i = contagem; i >= 1; i--) {
+            System.out.println( i + "...");
+            Thread.sleep(1000);
+
+
+        }
+
+
+
+
+
+
+    }
+}
