9275bac52989158ab5366ab04933d69a77117809
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 21:18:35 -0300

    adicionando exercicio07

---
+++ exercicio/src/exercicios07.java
@@ -0,0 +1,14 @@
+public class exercicios07 {
+    public static void main(String[] args) {
+
+        int num = 123;
+        int c = num / 100;
+        int d= (num / 10) % 10;
+        int u= num % 10;
+        System.out.println( c + d + u);
+
+
+
+
+    }
+}
