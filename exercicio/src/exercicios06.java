42081d08478a20ebbad4b0237a64f66eb0214a2d
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 21:15:56 -0300

    adicionando exerciocio06

---
+++ exercicio/src/exercicios06.java
@@ -0,0 +1,16 @@
+public class exercicios06 {
+    public static void main(String[] args) {
+
+        int n= 5;
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+
+
+        System.out.println(fatorial);
+
+
+
+    }
+}
