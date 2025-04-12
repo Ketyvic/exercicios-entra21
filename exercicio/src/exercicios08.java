132dc52c89a4a41604510e99f71676d7961b7cb5
KETLYN VICENTE <ketlyn.vicente@alunos.sc.senac.br>
2025-04-11 21:26:41 -0300

    adicionando exercicio08

---
+++ exercicio/src/exercicios08.java
@@ -0,0 +1,17 @@
+public class exercicios08 {
+    public static void main(String[] args) {
+
+
+        int num = 4;
+        if(num / ) {
+            System.out.println("numero primo");
+        }else{
+            System.out.println("nao é primo");
+        }
+
+
+
+
+
+    }
+}
